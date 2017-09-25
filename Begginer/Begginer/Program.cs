573162dc6d3720721964b89de3bcf17ca8657ae3
Анастасия <@DESKTOP-FPKO657>
2017-09-25 22:08:30 +0300

    Второе, но условия только на то, что число именно 4хзначное. Пока без дробных чисел

--- Begginer/Begginer/Program.cs
+++ Begginer/Begginer/Program.cs
@@ -14,7 +14,7 @@ namespace Begginer
             float n = Convert.ToInt32(Console.ReadLine());
             while (n < 0)
             {
-                Console.WriteLine("Недопустимый формат. Введите чило ещё раз:");
+                Console.WriteLine("Недопустимый формат. Введите чиcло ещё раз:");
                 n = Convert.ToInt32(Console.ReadLine());
             }
             if (n > 0)
@@ -26,12 +26,19 @@ namespace Begginer
             Console.WriteLine("\n\n\n");
             Console.WriteLine("Деление числа на разряды\n\nВведите 4хзначное число: \n");
             int r = Convert.ToInt32(Console.ReadLine());
-            int e = r % 10;
-            int d = r % 100; int d2 = d / 10; 
-            int s = r % 1000; int s2 = s / 100; 
-            int t = r / 1000;
-            Console.WriteLine("В этом числе {0} единиц, {1} десятков, {2} сотен и {3} тысяч", e, d2, s2, t);
-
+            while ((r > 9999)|(r < 1000))
+            {
+                Console.WriteLine("Недопустимый формат. Введите чило ещё раз:");
+                r = Convert.ToInt32(Console.ReadLine());
+            }
+            if ((r <= 9999) & (r >= 1000))
+            {
+                int e = r % 10;
+                int d = r % 100; int d2 = d / 10;
+                int s = r % 1000; int s2 = s / 100;
+                int t = r / 1000;
+                Console.WriteLine("\nВ этом числе {0} единиц, {1} десятков, {2} сотен и {3} тысяч", e, d2, s2, t);
+            }
 
             Console.ReadLine();
         }
