dde695d002c23c6fd3dd3747f9fc954b597ac920
Анастасия <@DESKTOP-FPKO657>
2017-09-25 21:54:56 +0300

    Первое

--- Begginer/Begginer/Program.cs
+++ Begginer/Begginer/Program.cs
@@ -12,9 +12,16 @@ namespace Begginer
         {
             Console.WriteLine("Перевод М в КМ\n\nВведите число (в М), которое нужно перевести в КМ: \n");
             float n = Convert.ToInt32(Console.ReadLine());
-            float m = n / 1000;
-            Console.WriteLine("{0} метров это {1} километров", n, m);
-         
+            while (n < 0)
+            {
+                Console.WriteLine("Недопустимый формат. Введите чило ещё раз:");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            if (n > 0)
+            {
+                float m = n / 1000;
+                Console.WriteLine("{0} метров это {1} километров", n, m);
+            }
 
             Console.WriteLine("\n\n\n");
             Console.WriteLine("Деление числа на разряды\n\nВведите 4хзначное число: \n");
@@ -23,7 +30,8 @@ namespace Begginer
             int d = r % 100; int d2 = d / 10; 
             int s = r % 1000; int s2 = s / 100; 
             int t = r / 1000;
-            Console.WriteLine("В этом числе {0} единиц, {1} десятков, {2} сотен и {3} тысяч", e, d2, s2, t);        
+            Console.WriteLine("В этом числе {0} единиц, {1} десятков, {2} сотен и {3} тысяч", e, d2, s2, t);
+
 
             Console.ReadLine();
         }
