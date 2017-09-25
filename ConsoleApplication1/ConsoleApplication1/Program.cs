45393ec18332caaa4bb812569b443f460944bcca
Анастасия <@DESKTOP-FPKO657>
2017-09-25 19:26:27 +0300

    Не знаю где ошибка( Сашенька помоги, пожалуйста

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two letters");
+            Console.WriteLine("");
+            int a = 5;
+            int b = 6;
+            int result = a + b;
+            Console.WriteLine(result);
+            Console.ReadKey();//here 
+        }
+    }
+}
