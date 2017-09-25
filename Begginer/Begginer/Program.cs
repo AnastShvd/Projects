1497efd9a8ee5fabe74639ac6a351017384dc451
Анастасия <@DESKTOP-FPKO657>
2017-09-25 22:32:15 +0300

    Временно

--- Begginer/Begginer/Program.cs
+++ Begginer/Begginer/Program.cs
@@ -25,19 +25,26 @@ namespace Begginer
 
             Console.WriteLine("\n\n\n");
             Console.WriteLine("Деление числа на разряды\n\nВведите 4хзначное число: \n");
-            int r = Convert.ToInt32(Console.ReadLine());
-            while ((r > 9999)|(r < 1000))
+            double r = Convert.ToDouble(Console.ReadLine());
+            while ((r % 1 != 0)||(r > 9999)||(r < 1000))
             {
-                Console.WriteLine("Недопустимый формат. Введите чило ещё раз:");
-                r = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Недопустимый формат. Введите число ещё раз:");
+                r = Convert.ToDouble(Console.ReadLine());
             }
-            if ((r <= 9999) & (r >= 1000))
+            if ((r % 1 == 0) && (r <= 9999) && (r >= 1000))
             {
-                int e = r % 10;
-                int d = r % 100; int d2 = d / 10;
-                int s = r % 1000; int s2 = s / 100;
-                int t = r / 1000;
-                Console.WriteLine("\nВ этом числе {0} единиц, {1} десятков, {2} сотен и {3} тысяч", e, d2, s2, t);
+                double e = r % 10;
+                double d = r % 100;
+                int d2 = Convert.ToInt32(d);
+                int d3 = d2 / 10;
+                double s = r % 1000;
+                int s2 = Convert.ToInt32(s);
+                int s3 = s2 / 100;
+                double t = r / 1000;
+                int t2 = Convert.ToInt32(t);
+                int t3 = t2 / 1000;
+
+                Console.WriteLine("В этом числе {0} единиц, {1} десятков, {2} сотни и {3} тысяч", e, d3, s3, t3);
             }
 
             Console.ReadLine();
