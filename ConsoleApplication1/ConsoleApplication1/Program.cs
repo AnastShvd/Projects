ecc22b640f75fd310135874cb83aa6366cc2e4c1
Alexander <Alexander@WIN-P7V0JSUPBKU>
2017-09-25 19:32:05 +0300

    Исправил все ошибки

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,13 +10,15 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter two letters");
-            Console.WriteLine("");
-            int a = 5;
-            int b = 6;
+            Console.WriteLine("Enter two digits");
+            
+            Console.WriteLine("Enter 1");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2");
+            int b = Convert.ToInt32(Console.ReadLine());
             int result = a + b;
             Console.WriteLine(result);
-            Console.ReadKey();//here 
+            Console.ReadKey();//here was a mistake (это пример изменения примера)
         }
     }
 }
